ecaee8331b1c3f1c39fd8ef7158ab882de47c238
Martín Silva Molina <msilvamolina@gmail.com>
2026-04-09 11:58:53 +0200

    feat: add turbopack configuration to next.config.ts
    - Introduced a turbopack configuration with the root set to the current directory in next.config.ts to enhance build performance.

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
+  turbopack: {
+    root: __dirname,
+  },
 
   images: {
     remotePatterns: [
